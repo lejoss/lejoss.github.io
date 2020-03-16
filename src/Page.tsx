ace7c07b645802fcf427b2b48fba88a6d975ad86
Alejandro Baez <nativo.alejandro@gmail.com>
2020-03-16 15:41:57 -0500

    added try catch for asyc operations

--- src/Page.tsx
+++ src/Page.tsx
@@ -11,57 +11,64 @@ import { useLocalStorage } from './useLocalStorage';
 const initialState: PageState = { posts: [], users: {}, fetching: false };
 
 export const Page: React.FC = (): React.ReactElement => {
-	const [clientId, setClientId] = useLocalStorage('clientId');
-	const [storedData, setDataToStore] = useLocalStorage('data');
+    const [clientId, setClientId] = useLocalStorage('clientId');
+    const [storedData, setDataToStore] = useLocalStorage('data');
 
-	const [{ posts, users, fetching }, setState] = React.useState<PageState>(() => {
-		if (!isEmpty(storedData)) {
-			const data = JSON.parse(storedData);
-			return {
-				...initialState,
-				clientId,
-				posts: data.posts,
-				users: data.users,
-			}
-		}
+    const [{ posts, users, fetching }, setState] = React.useState<PageState>(() => {
+        if (!isEmpty(storedData)) {
+            const data = JSON.parse(storedData);
+            return {
+                ...initialState,
+                clientId,
+                posts: data.posts,
+                users: data.users,
+            }
+        }
 
-		return initialState;
-	});
+        return initialState;
+    });
 
-	const fetchData = async () => {
-		setState({ ...initialState, fetching: true });
-		const posts = await axios.get(postsResource);
-		const users = await axios.get(usersResource);
+    const fetchData = async () => {
+        try {
+            setState({ ...initialState, fetching: true });
+            const posts = await axios.get(postsResource);
+            const users = await axios.get(usersResource);
 
-		setState({ posts: posts.data, users: mapKeys(users.data, 'id'), fetching: false });
-		const data = JSON.stringify({ posts: posts.data, users: mapKeys(users.data, 'id') });
-		setDataToStore(data);
-	}
+            setState({ posts: posts.data, users: mapKeys(users.data, 'id'), fetching: false });
+            const data = JSON.stringify({ posts: posts.data, users: mapKeys(users.data, 'id') });
+            setDataToStore(data);
 
-	React.useEffect(() => {
-		if (isEmpty(clientId)) {
-			setClientId(uuidv4())
-		}
-	}, [])
+        } catch (error) {
+            console.log(error);
+        }
 
-	React.useEffect(() => {
-		if (isEmpty(storedData)) {
-			fetchData();
-		}
-	}, []);
 
-	const renderPosts = posts.map((p: Post) => {
-		const post = {
-			id: p.id,
-			title: p.title,
-			body: p.body,
-			username: users[p.userId].username,
-			city: users[p.userId].address.city,
-		}
+    }
 
-		return <PostComponent key={post.id} post={post} />
-	});
+    React.useEffect(() => {
+        if (isEmpty(clientId)) {
+            setClientId(uuidv4())
+        }
+    }, [])
 
-	return fetching ? <h2 data-testid="loading">loading...</h2> : <>{renderPosts}</>
+    React.useEffect(() => {
+        if (isEmpty(storedData)) {
+            fetchData();
+        }
+    }, []);
+
+    const renderPosts = posts.map((p: Post) => {
+        const post = {
+            id: p.id,
+            title: p.title,
+            body: p.body,
+            username: users[p.userId].username,
+            city: users[p.userId].address.city,
+        }
+
+        return <PostComponent key={post.id} post={post} />
+    });
+
+    return fetching ? <h2 data-testid="loading">loading...</h2> : <>{renderPosts}</>
 
 }
