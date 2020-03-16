4f0e15e238f46b939a689af6637a92aef6f18600
Alejandro Baez <nativo.alejandro@gmail.com>
2020-03-16 13:20:54 -0500

    Updates

---
+++ precache-manifest.082e0f85b0dff2633e136bd5ec8bd10b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5331e4aa4632f137321c41f5177ae359",
+    "url": "/lejoss/lejoss.github.io/index.html"
+  },
+  {
+    "revision": "03191225d8cfbc3388ff",
+    "url": "/lejoss/lejoss.github.io/static/js/2.52cdc831.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lejoss/lejoss.github.io/static/js/2.52cdc831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6716e41bb6d99f2f6f65",
+    "url": "/lejoss/lejoss.github.io/static/js/main.facdebe4.chunk.js"
+  },
+  {
+    "revision": "87f05b9bdeff529d832d",
+    "url": "/lejoss/lejoss.github.io/static/js/runtime-main.509fdf19.js"
+  }
+]);
